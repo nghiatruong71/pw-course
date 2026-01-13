13c6b45fafed1411b02198e123249d051716fedc
Truong Trong Nghia <truongtrongnghia@Truongs-MacBook-Air.local>
2026-01-13 17:53:02 +0700

    add js exercises for ls 02

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,11 @@
+const number = 12;
+
+let name = “my number”;
+
+let isEven = false;
+
+if (number % 2 === 0) {
+
+    isEven = true;
+
+};
---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,15 @@
+const height = 173;
+
+if (height > 100) {
+
+    const sole = height % 100;
+
+    const ideal = sole * 9 / 10;
+
+    const max = sole;
+
+    const min = sole * 8 / 10;
+
+    `console.log("Ly tuong:", ideal, "Toi da:", max, "Toi thieu:", min);`
+
+}
